1a06873915833e3b48f155b1944c02811d242ac6
Damien Le Dantec <damien.ledantec@telecomsante.com>
2022-05-11 22:18:12 +0200

    [FEA] Add title and subtitle

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { Container } from '@mui/material';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <p>Hello world</p>
+    <Container>
+      <Header />
+    </Container>
   );
 }
 
